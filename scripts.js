39fab2d942c8123fbc1c8bb5311c3443d5764d68
shubha <shubharanjanmishra3@gmail.com>
2024-11-19 10:38:08 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// JavaScript for buttons and links interactions
+
+// Function for register button click
+function register() {
+    alert("Redirecting to registration page...");
+    // Here you can add the actual URL redirection if needed
+    window.location.href = "#";
+}
+
+// Function for learn more button click
+function learnMore() {
+    alert("Redirecting to the Programs and Degrees page...");
+    // Here you can add the actual URL redirection if needed
+    window.location.href = "#";
+}
